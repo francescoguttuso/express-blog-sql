621b2f76b4484e19588897e43d130cb3a95d98cd
francesco-guttuso <francescoguttuso@gmail.com>
2026-09-25 01:51:13 +0200

    feat: establish database connection and create index.js file

--- db.js
+++ db.js
@@ -1,1 +1,10 @@
-const mysql2;
+import mysql from "mysql2/promise.js";
+
+export const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "blog",
+});
+
+console.log("Database connesso!");
