704a8dce2aa153ac55bfd62f8f279517d4f7f230
francesco-guttuso <francescoguttuso@gmail.com>
2026-09-25 02:51:29 +0200

    feat: implement GET endpoint for fetching posts from the database

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
-import "./db.js";
+import { connection } from "./db.js";
 
 const app = express();
+
+app.get("/posts", async (req, res) => {
+  const [results] = await connection.query("SELECT * FROM posts");
+
+  res.json(results);
+});
 
 app.listen(3000, () => {
   console.log("Server avviato sulla porta 3000");
